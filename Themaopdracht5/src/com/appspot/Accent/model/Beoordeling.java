62f72cd2adcd050f1bbeae9684a83f06e2945e6a
Woutergrinsven <woutergrinsven@gmail.com>
2013-10-28 12:58:46 +0100

    beoordeling.java
    Een klein begin gemaakt aan beoordeling .java

--- Themaopdracht5/src/com/appspot/Accent/model/Beoordeling.java
+++ Themaopdracht5/src/com/appspot/Accent/model/Beoordeling.java
@@ -1,5 +1,10 @@
 package com.appspot.Accent.model;
 
 public class Beoordeling {
+	private ArrayList<Competentie> competenties = new ArrayList<Competentie>();
+	private Date datum;
+	public Beoordeling(Date dt){
+		datum = dt;
+	}
 
 }
