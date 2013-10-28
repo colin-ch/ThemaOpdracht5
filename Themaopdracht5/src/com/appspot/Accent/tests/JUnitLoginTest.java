1b729287810ba1aaa8d5d8d37d9842ceedd00b74
Woutergrinsven <woutergrinsven@gmail.com>
2013-10-28 12:22:20 +0100

    Test1 toegevoegd

---
+++ Themaopdracht5/src/com/appspot/Accent/tests/JUnitLoginTest.java
@@ -0,0 +1,26 @@
+public class JUnitLoginTest {
+	
+	@Before
+	public void setup(){
+		ArrayList<User> allUsers = new ArrayList<User>();
+		allUsers = (ArrayList<User>) getServletContext().getAttribute("users");
+	}
+	
+	// testcase inloggen A1.1/A1.2
+	@Test
+	public void testLoginSucces() {
+		User u = new User("123", "123", "123@gmail.com");
+		boolean loginsucces = false;
+		try {
+			for (User us : allUsers) {
+				if (u.equals(us)) {
+					loginsucces = true;
+				}
+			}
+		} catch (Exception e) {
+			fail("Onjuiste gebruikersnaam/wachtwoord combinatie");
+		}
+
+	}
+
+}
