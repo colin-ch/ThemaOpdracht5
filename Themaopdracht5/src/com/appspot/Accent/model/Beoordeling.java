ec7dd73c34d78b70890703a74ec90f30756bff6c
Woutergrinsven <woutergrinsven@gmail.com>
2013-10-29 15:03:41 +0100

    beoordeling update

--- Themaopdracht5/src/com/appspot/Accent/model/Beoordeling.java
+++ Themaopdracht5/src/com/appspot/Accent/model/Beoordeling.java
@@ -6,8 +6,29 @@ import java.util.Date;
 public class Beoordeling {
 	private ArrayList<Competentie> competenties = new ArrayList<Competentie>();
 	private Date datum;
-	public Beoordeling(Date dt){
+	private int waarde;
+	private String opmerking;
+	
+	public Beoordeling(Date dt, int wa, String op){
 		datum = dt;
+		waarde = wa;
+		opmerking = op;
+	}
+	
+	public int getWaarde() {
+		return waarde;
+	}
+
+	public void setWaarde(int waarde) {
+		this.waarde = waarde;
+	}
+
+	public String getOpmerking() {
+		return opmerking;
+	}
+
+	public void setOpmerking(String opmerking) {
+		this.opmerking = opmerking;
 	}
 
 }
