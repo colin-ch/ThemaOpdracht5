a9938f50040f71d7f6f55f6f92f0b58ecadbf0d6
DiThomas <thomasfcu@gmail.com>
2013-10-29 15:57:41 +0100

    UPDATE model klasse super epic big time ver 1.2

--- Themaopdracht5/src/com/appspot/Accent/model/School.java
+++ Themaopdracht5/src/com/appspot/Accent/model/School.java
@@ -1,10 +1,19 @@
 package com.appspot.Accent.model;
 
+import java.util.ArrayList;
+
 public class School {
 	private String naam;
 	private String adres;
 	private String plaats;
 	private String postCode;
+	private ArrayList<StageBedrijf> bedrijven = new ArrayList<StageBedrijf>();
+	private ArrayList<Leerling> leerlingen = new ArrayList<Leerling>();
+	private ArrayList<Docent> docenten = new ArrayList<Docent>();
+	private ArrayList<StageBegeleider> begeleiders = new ArrayList<StageBegeleider>();
+	private ArrayList<Stage> Stages = new ArrayList<Stage>();
+	
+	
 	
 	public School(String nm, String ad, String pl, String pc){
 		setNaam(nm);
@@ -13,6 +22,48 @@ public class School {
 		setPostCode(pc);
 	}
 
+	
+	public ArrayList<StageBedrijf> getBedrijven() {
+		return bedrijven;
+	}
+
+	public void setBedrijven(ArrayList<StageBedrijf> bedrijven) {
+		this.bedrijven = bedrijven;
+	}
+
+	public ArrayList<Leerling> getLeerlingen() {
+		return leerlingen;
+	}
+
+	public void setLeerlingen(ArrayList<Leerling> leerlingen) {
+		this.leerlingen = leerlingen;
+	}
+
+	public ArrayList<Docent> getDocenten() {
+		return docenten;
+	}
+
+	public void setDocenten(ArrayList<Docent> docenten) {
+		this.docenten = docenten;
+	}
+
+	public ArrayList<StageBegeleider> getBegeleiders() {
+		return begeleiders;
+	}
+
+	public void setBegeleiders(ArrayList<StageBegeleider> begeleiders) {
+		this.begeleiders = begeleiders;
+	}
+
+	public ArrayList<Stage> getStages() {
+		return Stages;
+	}
+
+	public void setStages(ArrayList<Stage> stages) {
+		Stages = stages;
+	}
+
+	
 	public String getNaam() {
 		return naam;
 	}
