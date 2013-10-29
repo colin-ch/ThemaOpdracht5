95705dac42a4641c8a2520ebc4ea6617ed05811c
DiThomas <thomasfcu@gmail.com>
2013-10-29 14:31:34 +0100

    Update model klasse

--- Themaopdracht5/src/com/appspot/Accent/model/StageOpleider.java
+++ Themaopdracht5/src/com/appspot/Accent/model/StageOpleider.java
@@ -1,8 +1,8 @@
 package com.appspot.Accent.model;
 
-public class StageBedrijf extends User{
+public class StageOpleider extends User{
 
-	public StageBedrijf(String us, String pw, String em) {
+	public StageOpleider(String us, String pw, String em) {
 		super(us, pw, em);
 		// TODO Auto-generated constructor stub
 	}
