cdf64b3b58178041d365cb0592615fce36589c5c
DiThomas <thomasfcu@gmail.com>
2013-10-29 13:06:39 +0100

    Leerling klasse update

--- Themaopdracht5/src/com/appspot/Accent/model/Leerling.java
+++ Themaopdracht5/src/com/appspot/Accent/model/Leerling.java
@@ -1,8 +1,15 @@
 package com.appspot.Accent.model;
 
+import java.util.Date;
+
 public class Leerling extends User{
 	private String klas;
 	private Docent docent;
+	private Date geboortedatum;
+	private int leeftijd;
+	private String roepnaam;
+	private St
+	
 
 	public Leerling(String us, String pw, String em, String kl) {
 		super(us, pw, em);
