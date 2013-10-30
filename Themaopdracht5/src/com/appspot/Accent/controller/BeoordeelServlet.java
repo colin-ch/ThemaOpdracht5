e55432cb3f027cbe379a65d165976417b7bad01a
DiThomas <thomasfcu@gmail.com>
2013-10-30 13:14:29 +0100

    Update Beoordelen

--- Themaopdracht5/src/com/appspot/Accent/controller/BeoordeelServlet.java
+++ Themaopdracht5/src/com/appspot/Accent/controller/BeoordeelServlet.java
@@ -9,19 +9,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hamcrest.core.IsInstanceOf;
+
 import com.appspot.Accent.model.Leerling;
 import com.appspot.Accent.model.Stage;
+import com.appspot.Accent.model.User;
 
 public class BeoordeelServlet extends HttpServlet{
-	protected void doPost( HttpServletRequest req, HttpServletResponse resp) 
+	protected void doGet( HttpServletRequest req, HttpServletResponse resp) 
 			throws ServletException, IOException{
 		RequestDispatcher rd = null;
-		Leerling userobject = (Leerling) req.getSession().getAttribute("userobject");
+		User userobject = (User) req.getSession().getAttribute("userobject");
+		Leerling leerlingObject = null;
 		
 		ArrayList<Stage> allStages = (ArrayList<Stage>) getServletContext().getAttribute("stages");
 		
+		
+		if(userobject instanceof Leerling){
+			leerlingObject = (Leerling) userobject;
+		}
+		
 		for(Stage s : allStages){
-			if(s.getDeLeerling().getUsername().equals(userobject.getUsername())){
+			
+			if(s.getDeLeerling().getUsername().equals(leerlingObject.getUsername())){
 				rd = req.getRequestDispatcher("www.google.com");
 
 			}
