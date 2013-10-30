14bc1745c6f44554cfe5bb5436bf10dc0b207073
Woutergrinsven <woutergrinsven@gmail.com>
2013-10-30 12:56:24 +0100

    update overzicht

--- Themaopdracht5/src/com/appspot/Accent/controller/OverzichtServlet.java
+++ Themaopdracht5/src/com/appspot/Accent/controller/OverzichtServlet.java
@@ -3,6 +3,7 @@ package com.appspot.Accent.controller;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,23 +22,34 @@ public class OverzichtServlet extends HttpServlet{
 		ArrayList<Stage>stages = new ArrayList<Stage>();
 		School s = new School("Accent", "Nijkerk", "Nijkerk", "0000AS");
 		stages = s.getStages();
+		boolean succes = false;
 		
 		for(Stage st : stages){
 			if(st.getDeLeerling().equals(l)){
 				ArrayList<Beoordeling>beoordelingen = new ArrayList<Beoordeling>();
 				beoordelingen = st.getBeoordelingen();
 				if(beoordelingen != null){
+					String bericht = null;
 					for(Beoordeling b : beoordelingen){
-						System.out.println(b);
+						bericht = bericht + b;
+						req.setAttribute("msgs", bericht);
 					}
+					succes = true;
 				}
 				else{
-					System.out.println("Er zijn nog geen beoordelingen gedaan");
+					req.setAttribute("msgs", "Er zijn nog geen beoordelingen gedaan");
 				}
 			}
 			else{
-				System.out.println("Er is nog geen stage bekend");
+				req.setAttribute("msgs", "Er is nog geen stage bekend");
 			}
+		}
+		RequestDispatcher rd = null;
+		if(succes){
+			rd = req.getRequestDispatcher("overzicht.jsp");
+		}
+		else{
+			req.setAttribute("msgs", "Er zijn nog geen beoordelingen gedaan");
 		}
 		
 	}
