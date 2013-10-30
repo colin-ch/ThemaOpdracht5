24071777c6efff1de43694ecf8cb0efb5b4e1f4b
Woutergrinsven <woutergrinsven@gmail.com>
2013-10-30 11:42:39 +0100

    overzicht servlet update

--- Themaopdracht5/src/com/appspot/Accent/controller/OverzichtServlet.java
+++ Themaopdracht5/src/com/appspot/Accent/controller/OverzichtServlet.java
@@ -8,9 +8,37 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.appspot.Accent.model.Beoordeling;
+import com.appspot.Accent.model.Leerling;
+import com.appspot.Accent.model.School;
+import com.appspot.Accent.model.Stage;
+
 public class OverzichtServlet extends HttpServlet{
 	protected void doPost( HttpServletRequest req, HttpServletResponse resp) 
 			throws ServletException, IOException{
+		
+		Leerling l = (Leerling) req.getSession().getAttribute("userobject");
+		ArrayList<Stage>stages = new ArrayList<Stage>();
+		School s = new School("Accent", "Nijkerk", "Nijkerk", "0000AS");
+		stages = s.getStages();
+		
+		for(Stage st : stages){
+			if(st.getDeLeerling().equals(l)){
+				ArrayList<Beoordeling>beoordelingen = new ArrayList<Beoordeling>();
+				beoordelingen = st.getBeoordelingen();
+				if(beoordelingen != null){
+					for(Beoordeling b : beoordelingen){
+						System.out.println(b);
+					}
+				}
+				else{
+					System.out.println("Er zijn nog geen beoordelingen gedaan");
+				}
+			}
+			else{
+				System.out.println("Er is nog geen stage bekend");
+			}
+		}
 		
 	}
 
