d62ab4f18f6c74a977ebba3fa9203ee216a1a481
Woutergrinsven <woutergrinsven@gmail.com>
2013-10-30 21:10:21 +0100

    update overzicht
    check de comments die ik er bij heb gezet

--- Themaopdracht5/src/com/appspot/Accent/controller/BeoordelingOphalenServlet.java
+++ Themaopdracht5/src/com/appspot/Accent/controller/BeoordelingOphalenServlet.java
@@ -3,14 +3,22 @@ package com.appspot.Accent.controller;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 import java.util.logging.Logger;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.appspot.Accent.model.Beoordeling;
+import com.appspot.Accent.model.Competentie;
+import com.appspot.Accent.model.Leerling;
+import com.appspot.Accent.model.Stage;
+import com.appspot.Accent.model.Stelling;
 
 public class BeoordelingOphalenServlet extends HttpServlet{
  
@@ -26,7 +34,39 @@ private static final Logger log = Logger.getLogger(OverzichtServlet.class.getNam
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		Object o = req.getSession().getAttribute("userobject");
 		
+		ArrayList<Stage>stages = new ArrayList<Stage>();
+		stages = (ArrayList<Stage>) getServletContext().getAttribute("stages");
+		
+		if(o instanceof Leerling ){//kijkt of het een leerling is
+			for(Stage st : stages){// haalt alle stages op
+				if (st.getDeLeerling().getUsername().equals( ((Leerling) o).getUsername())){ // hier kijkt hij welke stage bij de leerlinge hoort
+					ArrayList<Beoordeling>beoordelingen = new ArrayList<Beoordeling>();
+					beoordelingen = st.getBeoordelingen();
+					for(Beoordeling b:beoordelingen){ //alle beoordelingen ophalen
+						System.out.println(b.getDatum());//datums niet gelijk
+						System.out.println(d);//d = tue jan 01 00:00:00 CET 2013 en b.getDatum = 01/01/2013
+						if(b.getDatum().equals(d));{//datums vergelijken
+							ArrayList<Competentie>competenties = new ArrayList<Competentie>();
+							competenties = b.getCompetenties();
+							for(Competentie c: competenties){//competenties doorlopen
+								ArrayList<Stelling> deStellingen = new ArrayList<Stelling>();
+								deStellingen = c.getDeStellingen();
+								for(Stelling s : deStellingen){ //stellingen door lopen
+									System.out.println(s.getDeWaarde());
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		
+		RequestDispatcher rd = null;
+		rd = req.getRequestDispatcher("beoordelingoverzicht.jsp");
+		rd.forward(req, resp);
 		
 	}
 	
