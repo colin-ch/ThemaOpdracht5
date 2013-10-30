cfdaee2c6cf80ce48d67fac334ba607c81562120
DiThomas <thomasfcu@gmail.com>
2013-10-30 10:38:27 +0100

    Update webpages (leerling beoordelen)

--- Themaopdracht5/src/com/appspot/Accent/model/Beoordeling.java
+++ Themaopdracht5/src/com/appspot/Accent/model/Beoordeling.java
@@ -6,9 +6,26 @@ import java.util.Date;
 public class Beoordeling {
 	private ArrayList<Competentie> competenties = new ArrayList<Competentie>();
 	private Date datum;
-	private int waarde;
+	private int waarde;	
 	private String opmerking;
 	
+	public ArrayList<Competentie> getCompetenties() {
+		return competenties;
+	}
+
+	public void setCompetenties(ArrayList<Competentie> competenties) {
+		this.competenties = competenties;
+	}
+
+	public Date getDatum() {
+		return datum;
+	}
+
+	public void setDatum(Date datum) {
+		this.datum = datum;
+	}
+
+
 	public Beoordeling(Date dt, int wa, String op){
 		datum = dt;
 		waarde = wa;
