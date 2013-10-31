e5ae43c9b05ec104d47caf2e3fcfd3b5c2ed2b1b
leonelescantre <leon.dekanter@gmail.com>
2013-10-31 22:38:20 +0100

    Beoordeling update

--- Themaopdracht5/src/com/appspot/Accent/controller/BeoordeelServlet.java
+++ Themaopdracht5/src/com/appspot/Accent/controller/BeoordeelServlet.java
@@ -16,27 +16,34 @@ import com.appspot.Accent.model.Stage;
 import com.appspot.Accent.model.Stelling;
 
 public class BeoordeelServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private static final Logger log = Logger.getLogger(BeoordeelServlet.class
 			.getName());
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		boolean succes = false;
+		
+		ArrayList<Integer> allWaardes = new ArrayList<Integer>();
+		int teller = 0;
+		int stellingTeller = Integer.parseInt(req.getParameter("stellingTeller"));
+		while(teller != stellingTeller){
+			teller++;
+			allWaardes.add(Integer.parseInt(req.getParameter("waarde"+teller)));
 		
 		
-		int waarde1 = Integer.parseInt(req.getParameter("waarde1"));
-		int waarde2 = Integer.parseInt(req.getParameter("waarde2"));
-		int waarde3 = Integer.parseInt(req.getParameter("waarde3"));
-		int waarde4 = Integer.parseInt(req.getParameter("waarde4"));
-		int waarde5 = Integer.parseInt(req.getParameter("waarde5"));
+	
 		
-		RequestDispatcher rd = null;
-		Object o = req.getSession().getAttribute("userobject");
+		
+		/*Object o = req.getSession().getAttribute("userobject");
 
 		ArrayList<Stage> allStages = (ArrayList<Stage>) getServletContext()
 				.getAttribute("stages");
 
 		for (Stage s : allStages) {
-
 			if (o instanceof Leerling) {
 				if (s.getDeLeerling().getUsername()
 						.equals(((Leerling) o).getUsername())) {
@@ -46,11 +53,24 @@ public class BeoordeelServlet extends HttpServlet {
 					
 				}
 			}
-			rd = req.getRequestDispatcher("BeoordelenLeerling.jsp");
-
+			*/
+		System.out.println(allWaardes);
+		req.setAttribute("msgs", "allWaardes");
+		RequestDispatcher rd = null;
+		if(allWaardes !=null){
+			succes=true;
 		}
-		rd.forward(req, resp);
+		if(succes){
+			req.setAttribute("msgs", "Succes");
+			rd = req.getRequestDispatcher("overzicht.do");
+		}
+		else{
+			rd = req.getRequestDispatcher("BeoordelenLeerling.jsp");
+		}
+		rd.forward(req, resp);	
 
+		//}
+	
 	}
-
+	}
 }
