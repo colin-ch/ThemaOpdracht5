b30533b74c1a4f16bc045ad8aaec9492443a7c0d
Woutergrinsven <woutergrinsven@gmail.com>
2013-11-04 13:39:17 +0100

    update beoordeling/beoordelingservlet

--- Themaopdracht5/src/com/appspot/Accent/controller/BeoordeelServlet.java
+++ Themaopdracht5/src/com/appspot/Accent/controller/BeoordeelServlet.java
@@ -2,10 +2,10 @@ package com.appspot.Accent.controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.logging.Logger;
 
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -27,157 +27,158 @@ public class BeoordeelServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-//		boolean succes = false;
-	RequestDispatcher rd = null;
+		//		boolean succes = false;
+		RequestDispatcher rd = null;
 		Object o = req.getSession().getAttribute("userobject");
-Beoordeling rate =null;
-Stage deStage = null;
-Competentie nr1 = null;
-Competentie nr2 = null;
-Stelling stelling1 = null;		
-
-Stelling stelling2 = null;		
-
-Stelling stelling3 = null;		
-
-Stelling stelling4 = null;		
-
-Stelling stelling5 = null;		
-ArrayList<Beoordeling> allBeoordelingen = new ArrayList<Beoordeling>();
-//		ArrayList<Integer> allWaardes = new ArrayList<Integer>();
-//		int teller = 0;
-//		int stellingTeller = Integer.parseInt(req.getParameter("stellingTeller"));
-//		while(teller != stellingTeller){
-//			teller++;
-//			allWaardes.add(Integer.parseInt(req.getParameter("waarde"+teller)));
+		Beoordeling rate =null;
+		Stage deStage = null;
+		Competentie nr1 = null;
+		Competentie nr2 = null;
+		Stelling stelling1 = null;		
 		
-
-		ArrayList<Stage> allStages = (ArrayList<Stage>) getServletContext()
-				.getAttribute("stages");
-		log.info("begin!");
-
+		Stelling stelling2 = null;		
 		
+		Stelling stelling3 = null;		
 		
-		if (req.getParameter("Opslaan") != null){
-			log.info("knop opslaan");
+		Stelling stelling4 = null;		
+		
+		Stelling stelling5 = null;		
+		ArrayList<Beoordeling> allBeoordelingen = new ArrayList<Beoordeling>();
+		//		ArrayList<Integer> allWaardes = new ArrayList<Integer>();
+		//		int teller = 0;
+		//		int stellingTeller = Integer.parseInt(req.getParameter("stellingTeller"));
+		//		while(teller != stellingTeller){
+		//			teller++;
+		//			allWaardes.add(Integer.parseInt(req.getParameter("waarde"+teller)));
+			
+		
+			ArrayList<Stage> allStages = (ArrayList<Stage>) getServletContext().getAttribute("stages");
+			log.info("begin!");
+			
+			if (req.getParameter("Opslaan") != null){
+				log.info("knop opslaan");
+				for (Stage s : allStages) {
+					if (o instanceof Leerling) {
+						log.info("ingelogd als leerling");
+		
+						if (s.getDeLeerling().getUsername()
+								.equals(((Leerling) o).getUsername())) {
+							log.info("de juiste leerling");
+		
+							allBeoordelingen =
+							 s.getBeoordelingen();
+								log.info("beoordelingen " + allBeoordelingen);
+		
+								for(Beoordeling be : allBeoordelingen){
+									log.info("doorloop arraylist");
+								
+										if(be.getDatum() == null){
+											log.info("geen datum");
+								
+										ArrayList<Competentie> competenties = be.getCompetenties() ;
+										
+										log.info("competenite " + competenties);
+								
+										int teller = 0;
+										for(Competentie c : competenties){
+											ArrayList<Stelling> stellingen = new ArrayList<Stelling>();
+											stellingen = c.getDeStellingen();
+											
+											for(Stelling stel : stellingen){
+												teller++;
+								
+												stel.setDeWaarde(req.getParameter("waarde"+ teller));
+												log.info("waardes is" + req.getParameter("waarde"+ stel.getDeWaarde()) );
+											}
+											}
+										}
+										be.setOpmerking(req.getParameter("opmerking"));
+											log.info(be.getOpmerking());
+											Date d = new Date();
+											String datum = d.toString();
+											be.createBeoordeling(datum, be.getOpmerking());
+								}
+							}	
+						}
+					}
+					
+					
+					
+					String msgs = "<h4 class='alert_success'>U heeft de beoordeling successvol ingevuld.</h4>";
+		
+				req.setAttribute("msgs", msgs);
+				
+		
+				getServletContext().setAttribute("stages", allStages);
+				rd = req.getRequestDispatcher("index.jsp");
+			}
+			
+			
+			
+			
+			else{
+			
+		
 			for (Stage s : allStages) {
+				log.info("stage");
 				if (o instanceof Leerling) {
-					log.info("ingelogd als leerling");
-
 					if (s.getDeLeerling().getUsername()
 							.equals(((Leerling) o).getUsername())) {
-						log.info("de juiste leerling");
-
-						allBeoordelingen =
-						 s.getBeoordelingen();
-							log.info("beoordelingen " + allBeoordelingen);
-
-for(Beoordeling be : allBeoordelingen){
-	log.info("doorloop arraylist");
-
-			if(be.getDatum() == null){
-				log.info("geen datum");
-
-			ArrayList<Competentie> competenties = be.getCompetenties() ;
-			
-			log.info("competenite " + competenties);
-
-			int teller = 0;
-			for(Competentie c : competenties){
-				ArrayList<Stelling> stellingen = new ArrayList<Stelling>();
-				stellingen = c.getDeStellingen();
-				
-				for(Stelling stel : stellingen){
-					teller++;
-
-					stel.setDeWaarde(req.getParameter("waarde"+ teller));
-					log.info("waardes is" + req.getParameter("waarde"+ stel.getDeWaarde()) );
-				}
-				}
-			}
-			be.setOpmerking(req.getParameter("opmerking"));
-			log.info(be.getOpmerking());
-}
-			}	
+						 allBeoordelingen = s.getBeoordelingen();
+						 
+						 for(Beoordeling be : allBeoordelingen){
+								log.info("beoordeling");
+								
+		
+						 					// Te aant e passen na de core
+						 					nr1 = new Competentie(
+						 							"1. Samenwerken en overleggen");
+						 					nr2 = new Competentie(
+											"2. Aandacht en begrip tonen");
+						 
+						 				 stelling1 = new Stelling(
+						 						"de leerling gedraagt zich zo dat samenwerking makkelijk gaat",
+						 						null);
+						 				 stelling2 = new Stelling(
+												"De leerling past zich aan de groep aan", null);
+						 				 stelling3 = new Stelling(
+						 							"De leerling houdt zich aan de regels van het bedrijf",
+						 							null);
+						 					 stelling4 = new Stelling(
+						 							"de leerling luister naar een ander", null);
+						 					 stelling5 = new Stelling(
+						 						"de leerling laat de ander uitpraten", null);
+						 
+						 				
+						 					rate= be;
+						 					deStage = s;
+		
+						 }
+					}
 				}
 			}
 			
-			
-			
-			String msgs = "<h4 class='alert_success'>U heeft de beoordeling successvol ingevuld.</h4>";
-
-			req.setAttribute("msgs", msgs);
-
-			getServletContext().setAttribute("stages", allStages);
-			rd = req.getRequestDispatcher("index.jsp");
-		}
+			nr1.getDeStellingen().add(stelling1);
+				nr1.getDeStellingen().add(stelling2);
+				nr1.getDeStellingen().add(stelling3);
+				nr2.getDeStellingen().add(stelling4);
+				nr2.getDeStellingen().add(stelling5);
+				log.info("YOLO");
 		
+					
+		rate.getCompetenties().add(nr1);
+		rate.getCompetenties().add(nr2);					 				
+				deStage.setBeoordelingen(allBeoordelingen);
+				req.setAttribute("competenties", deStage);
 		
-		
-		
-		else{
-		
-	
-		for (Stage s : allStages) {
-			log.info("stage");
-			if (o instanceof Leerling) {
-				if (s.getDeLeerling().getUsername()
-						.equals(((Leerling) o).getUsername())) {
-					 allBeoordelingen = s.getBeoordelingen();
-					 
-					 for(Beoordeling be : allBeoordelingen){
-							log.info("beoordeling");
-							
-
-					 					// Te aant e passen na de core
-					 					nr1 = new Competentie(
-					 							"1. Samenwerken en overleggen");
-					 					nr2 = new Competentie(
-										"2. Aandacht en begrip tonen");
-					 
-					 				 stelling1 = new Stelling(
-					 						"de leerling gedraagt zich zo dat samenwerking makkelijk gaat",
-					 						null);
-					 				 stelling2 = new Stelling(
-											"De leerling past zich aan de groep aan", null);
-					 				 stelling3 = new Stelling(
-					 							"De leerling houdt zich aan de regels van het bedrijf",
-					 							null);
-					 					 stelling4 = new Stelling(
-					 							"de leerling luister naar een ander", null);
-					 					 stelling5 = new Stelling(
-					 						"de leerling laat de ander uitpraten", null);
-					 
-					 				
-					 					rate= be;
-					 					deStage = s;
-
-					 }
-				}
+			rd = req.getRequestDispatcher("BeoordelenLeerling.jsp");
 			}
-		}
-		
-		nr1.getDeStellingen().add(stelling1);
-			nr1.getDeStellingen().add(stelling2);
-			nr1.getDeStellingen().add(stelling3);
-			nr2.getDeStellingen().add(stelling4);
-			nr2.getDeStellingen().add(stelling5);
-			log.info("YOLO");
-
 			
-rate.getCompetenties().add(nr1);
-rate.getCompetenties().add(nr2);					 				
-		deStage.setBeoordelingen(allBeoordelingen);
-		req.setAttribute("competenties", deStage);
-
-		rd = req.getRequestDispatcher("BeoordelenLeerling.jsp");
-		}
+			rd.forward(req, resp);	
 		
-		rd.forward(req, resp);	
-
-		//}
-	
-//	}
+			//}
+		
+		//	}
 		
 	
 	
--- Themaopdracht5/src/com/appspot/Accent/model/Beoordeling.java
+++ Themaopdracht5/src/com/appspot/Accent/model/Beoordeling.java
@@ -1,13 +1,15 @@
 package com.appspot.Accent.model;
 
 import java.util.ArrayList;
-import java.util.Date;
+
+import com.googlecode.objectify.Objectify;
 
 public class Beoordeling {
 	
 	private ArrayList<Competentie> competenties = new ArrayList<Competentie>();
 	private String datum;
 	private String opmerking;
+	private Objectify ofy;
 
 	public Beoordeling(String dt, String op){
 		datum = dt;
@@ -36,6 +38,10 @@ public class Beoordeling {
 
 	public void setOpmerking(String opmerking) {
 		this.opmerking = opmerking;
+	}
+	public void createBeoordeling(String d, String opm){
+	       ofy.put(new Beoordeling(d, opm));
+
 	}
 
 
