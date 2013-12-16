e298dc0844b70c31fad6e21b8733b76c45458d3e
Woutergrinsven <woutergrinsven@gmail.com>
2013-12-16 14:50:44 +0100

    selenium login test

---
+++ Themaopdracht5/src/com/appspot/Accent/tests/LoginTest.java
@@ -0,0 +1,40 @@
+package com.appspot.Accent.tests;
+
+import com.thoughtworks.selenium.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.regex.Pattern;
+
+public class LoginTest{
+	private Selenium selenium;
+
+	@Before
+	public void setUp() throws Exception {
+		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://project-omega.appspot.com/");
+		selenium.start();
+	}
+
+	@Test
+	public void testLogin() throws Exception {
+		selenium.open("/");
+		selenium.type("name=username", "123");
+		selenium.type("name=password", "123");
+		selenium.click("css=input.btn.btn-success");
+		selenium.waitForPageToLoad("30000");
+		assertEquals("Dashboard I Admin Panel", selenium.getTitle());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+}
+
+
+
+
