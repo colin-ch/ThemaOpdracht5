ea0c934c08c87e2f7c28e9f27f3c23d6b8c960f1
Woutergrinsven <woutergrinsven@gmail.com>
2013-12-24 15:41:25 +0100

    gebruikersaanmakenservlet extra check

--- Themaopdracht5/src/com/appspot/Accent/tests/TestData.java
+++ Themaopdracht5/src/com/appspot/Accent/tests/TestData.java
@@ -1,30 +1,51 @@
 package com.appspot.Accent.tests;
 
-import java.util.ArrayList;
-import java.util.logging.Logger;
-
-import com.appspot.Accent.controller.BeoordelingAanmakenServlet;
-import com.appspot.Accent.model.Leerling;
-import com.appspot.Accent.model.service.LeerlingOfyDAOImpl;
-import com.googlecode.objectify.Objectify;
+import com.thoughtworks.selenium.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.regex.Pattern;
 
 public class TestData {
-	private Objectify ofy;
-	private static final Logger log = Logger.getLogger(BeoordelingAanmakenServlet.class.getName());
-	LeerlingOfyDAOImpl lod = new LeerlingOfyDAOImpl();
-	ArrayList<Leerling>leerlingen = (ArrayList<Leerling>) lod.getAllLeerlingen();
-	public TestData(){
-		
+	private Selenium selenium;
+
+	@Before
+	public void setUp() throws Exception {
+		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://project-omega.appspot.com/");
+		selenium.start();
 	}
-	public void aanmaken(){
-		Leerling l1 = new Leerling("123user", "123pass", null, null, null, null, null, null);
-		for(Leerling l : leerlingen){
-			if(l.getUsername().equals(l1.getUsername())){
-				log.info("gebruiker bestaat al");
-			}
-			else{
-				ofy.put(l1);
-			}
-		}
+
+	@Test
+	public void testTestdata() throws Exception {
+		selenium.open("/");
+		selenium.type("name=username", "123");
+		selenium.type("name=password", "123");
+		selenium.click("css=input.btn.btn-success");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=Beheerders pagina");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("css=button");
+		selenium.waitForPageToLoad("30000");
+		selenium.type("name=username", "abc");
+		selenium.type("name=wachtwoord", "abc");
+		selenium.type("name=email", "123@gmail.com");
+		selenium.type("name=klas", "1");
+		selenium.type("name=begeleiders", "123@gmail.com");
+		selenium.type("name=gebdat", "11-11-13");
+		selenium.type("name=roepnaam", "1");
+		selenium.type("name=achternaam", "1");
+		selenium.click("name=leerling");
+		selenium.waitForPageToLoad("30000");
+		
+		
+		
+		selenium.click("link=Loguit");
+		selenium.waitForPageToLoad("30000");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
 	}
 }
