6f16c226fd346f04e7646ceeed1c4380e2683264
Woutergrinsven <woutergrinsven@gmail.com>
2013-12-24 15:18:24 +0100

    beheerder.jsp

--- Themaopdracht5/src/com/appspot/Accent/tests/LoginTest.java
+++ Themaopdracht5/src/com/appspot/Accent/tests/LoginTest.java
@@ -2,40 +2,23 @@ package com.appspot.Accent.tests;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.appspot.Accent.model.Leerling;
-import com.appspot.Accent.model.service.LeerlingOfyDAOImpl;
-import com.googlecode.objectify.Objectify;
-import com.googlecode.objectify.ObjectifyService;
 import com.thoughtworks.selenium.DefaultSelenium;
 import com.thoughtworks.selenium.Selenium;
 
 public class LoginTest{
 	private Selenium selenium;
-	private Objectify ofy;
 
 	@Before
 	public void setUp() throws Exception {
-		LeerlingOfyDAOImpl lod = new LeerlingOfyDAOImpl();
-		ArrayList<Leerling>leerlingen = (ArrayList<Leerling>) lod.getAllLeerlingen();
-		Leerling l1 = new Leerling("123username", "123pass", null, null, null, null, null, null);
-		ObjectifyService.register(Leerling.class);
+		//TestData t = new TestData();
+		//t.aanmaken();
 		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://project-omega.appspot.com/");
 		selenium.start();
-		if(leerlingen != null){
-		for(Leerling l : leerlingen){
-			if(l.getUsername().equals(l1.getUsername()) && l.getPassword().equals(l1.getPassword())){
-			}
-			else{
-				ofy.put(l1);
-			}
-		}
-		}
+		
 	}
 
 	@Test
