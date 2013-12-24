87fcab9554d3fd294aaa27baa05e86365aada67d
DiThomas <thomasfcu@gmail.com>
2013-12-24 14:07:16 +0100

    Begin junit testen
    ylolo

---
+++ Themaopdracht5/src/com/appspot/Accent/tests/LoginjUnitTest.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.appspot.Accent.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author DiThomas
+ *
+ */
+public class LoginjUnitTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
