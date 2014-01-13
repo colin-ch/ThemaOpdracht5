d7b59b3d6b11725503a8fea7170cc129bf3c9a69
Woutergrinsven <woutergrinsven@gmail.com>
2014-01-13 13:17:45 +0100

    login fix

--- Themaopdracht5/src/com/appspot/Accent/controller/LoginServlet.java
+++ Themaopdracht5/src/com/appspot/Accent/controller/LoginServlet.java
@@ -39,10 +39,14 @@ public class LoginServlet extends HttpServlet {
 		DocentOfyDAOImpl d = new DocentOfyDAOImpl();
 		StageBedrijfOfyDAOImpl sb = new StageBedrijfOfyDAOImpl();
 		StageBegeleiderOfyDAOImpl sbg = new StageBegeleiderOfyDAOImpl();
+		ArrayList<Leerling>leerlingen = (ArrayList<Leerling>) l.getAllLeerlingen();
+		ArrayList<Docent>docenten = (ArrayList<Docent>) d.getAllDocenten();
+		ArrayList<StageBedrijf>bedrijven = (ArrayList<StageBedrijf>) sb.getAllStageBedrijven();
+		ArrayList<StageBegeleider>begeleiders = (ArrayList<StageBegeleider>) sbg.getAllBegeleiders();
 		// er wrodt gekeken pof de gegevens overeenkomt met een van de users er is verschil in type users, namelijk leerlingen en docenten etc.
 		
 		// er wordt gecontroleerd of het een leerling is 
-		for(Leerling le : l.getAllLeerlingen()){
+		for(Leerling le : leerlingen){
 			if(username.equals(le.getUsername()) && pass.equals(le.getPassword())){
 				loginsucces = true;
 				System.out.println("Is een leerling");
@@ -53,7 +57,7 @@ public class LoginServlet extends HttpServlet {
 		}
 		// er wordt gecontroleerd of het een docent is 
 
-		for(Docent de : d.getAllDocenten()){
+		for(Docent de : docenten){
 			if(username.equals(de.getUsername()) && pass.equals(de.getPassword()) && loginsucces == false){
 				loginsucces = true;
 				System.out.println("Is een docent");
@@ -64,7 +68,7 @@ public class LoginServlet extends HttpServlet {
 		}
 		// er wordt gecontroleerd of het een stagebedrijf/opleider is 
 
-		for(StageBedrijf sbe : sb.getAllStageBedrijven()){
+		for(StageBedrijf sbe : bedrijven){
 			if(username.equals(sbe.getUsername()) && pass.equals(sbe.getPassword()) && loginsucces == false){
 				loginsucces = true;
 				System.out.println("Is een stagebedrijf");
@@ -75,7 +79,7 @@ public class LoginServlet extends HttpServlet {
 		}
 		// er wordt gecontroleerd of het een begeleider is 
 
-		for(StageBegeleider sbge : sbg.getAllBegeleiders()){
+		for(StageBegeleider sbge : begeleiders){
 			if(username.equals(sbge.getUsername()) && pass.equals(sbge.getPassword()) && loginsucces == false){
 				loginsucces = true;
 				System.out.println("Is een stagebegeleider");
