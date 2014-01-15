1ee03edbf56e727ee35057920f007d968165b2b2
Woutergrinsven <woutergrinsven@gmail.com>
2014-01-15 14:12:20 +0100

    login test

--- Themaopdracht5/src/com/appspot/Accent/tests/LoginTest.java
+++ Themaopdracht5/src/com/appspot/Accent/tests/LoginTest.java
@@ -1,6 +1,6 @@
 package com.appspot.Accent.tests;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
@@ -22,14 +22,98 @@ public class LoginTest{
 	}
 
 	@Test
-	public void testLogin() throws Exception {
+	public void simpelLogintest() throws Exception {
 		selenium.open("/");
-		selenium.type("name=username", "123");
-		selenium.type("name=password", "123");
+		selenium.type("name=username", "abc");
+		selenium.type("name=password", "abc");
 		selenium.click("css=input.btn.btn-success");
 		selenium.waitForPageToLoad("30000");
-		assertEquals("Dashboard I Admin Panel", selenium.getTitle());
+		assertTrue("Dashboard I Admin Panel".equals(selenium.getTitle()));
+		selenium.click("link=Loguit");
+		selenium.waitForPageToLoad("30000");
 	}
+	@Test
+	public void spatieLoginTest() throws Exception{
+		selenium.open("/");
+		selenium.type("name=username", "abc abc");
+		selenium.type("name=password", "abcd");
+		selenium.click("css=input.btn.btn-success");
+		selenium.waitForPageToLoad("30000");
+		assertTrue("Dashboard I Admin Panel".equals(selenium.getTitle()));
+		selenium.click("link=Loguit");
+		selenium.waitForPageToLoad("30000");
+	}
+	@Test
+	public void aanhalingstekenLogintest() throws Exception{
+		selenium.open("/");
+		selenium.type("name=username", "abc\"abc");
+		selenium.type("name=password", "abcde");
+		selenium.click("css=input.btn.btn-success");
+		selenium.waitForPageToLoad("30000");
+		assertTrue("Dashboard I Admin Panel".equals(selenium.getTitle()));
+		selenium.click("link=Loguit");
+		selenium.waitForPageToLoad("30000");
+	}
+	@Test
+	public void spatie2Logintest() throws Exception{
+		selenium.open("/");
+		selenium.type("name=username", " abc");
+		selenium.type("name=password", "abcdef");
+		selenium.click("css=input.btn.btn-success");
+		selenium.waitForPageToLoad("30000");
+		assertTrue("Dashboard I Admin Panel".equals(selenium.getTitle()));
+		selenium.click("link=Loguit");
+		selenium.waitForPageToLoad("30000");
+	}
+	@Test
+	public void uitroeptekenLogintest() throws Exception{
+		selenium.open("/");
+		selenium.type("name=username", "cb!a");
+		selenium.type("name=password", "cba");
+		selenium.click("css=input.btn.btn-success");
+		selenium.waitForPageToLoad("30000");
+		assertTrue("Dashboard I Admin Panel".equals(selenium.getTitle()));
+		selenium.click("link=Loguit");
+		selenium.waitForPageToLoad("30000");
+	}
+	@Test
+	public void foutwwLogintest() throws Exception{
+		selenium.open("/LogoutServlet.do");
+		selenium.type("name=username", "abc");
+		selenium.type("name=password", "qwerty");
+		selenium.click("css=input.btn.btn-success");
+		selenium.waitForPageToLoad("30000");
+		assertTrue("Login".equals(selenium.getTitle()));
+	}
+
+	@Test
+	public void foutnameLogintest() throws Exception{
+		selenium.open("/");
+		selenium.type("name=username", "qwerty");
+		selenium.type("name=password", "abc");
+		selenium.click("css=input.btn.btn-success");
+		selenium.waitForPageToLoad("30000");
+		assertTrue("Login".equals(selenium.getTitle()));
+	}
+	@Test
+	public void hoofdletternaamLogintest() throws Exception{
+		selenium.open("/");
+		selenium.type("name=username", "ABC");
+		selenium.type("name=password", "abc");
+		selenium.click("css=input.btn.btn-success");
+		selenium.waitForPageToLoad("30000");
+		assertTrue("Login".equals(selenium.getTitle()));
+	}
+	@Test
+	public void hoofdletterwLogintest() throws Exception{
+		selenium.open("/");
+		selenium.type("name=username", "abc");
+		selenium.type("name=password", "ABC");
+		selenium.click("css=input.btn.btn-success");
+		selenium.waitForPageToLoad("30000");
+		assertTrue("Login".equals(selenium.getTitle()));
+	}
+	
 
 	@After
 	public void tearDown() throws Exception {
